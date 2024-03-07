0ec4357e6fc1a46ac2699a74be737f04960d16e8
Misha Treushnikov <treushnikovmv@ya.ru>
2024-03-07 23:11:39 +0300

    first

---
+++ .myenv/udp.py
@@ -0,0 +1,25 @@
+import socket
+
+UDP_IP = "10.1.1.16"
+UDP_PORT = 49002
+
+sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+sock.bind((UDP_IP,UDP_PORT))
+
+while True:
+    data, addr = sock.recvfrom(2048) # buffer size is 1024 bytes
+    #print("received message: %s" % data)
+    print(data.hex())
+    airData = hex(data[0])
+    if airData=='0x81':
+        print(airData)
+    typeMessage = bin(data[6]) # 7 байт тип сообщения -> первые шесть бит: тип АДСБ, два последних: номер канала
+    print(typeMessage)
+
+    # для первого канала 0b00001100 - ADSB-112; 0b00001000 - ADSB-56; 0b00000100 - A/C
+    # для второго канала 0b00001101 - ADSB-112; 0b00001001 - ADSB-56; 0b00000101 - A/C
+    # для третьего канала 0b00001110 - ADSB-112; 0b00001010 - ADSB-56; 0b00000110 - A/C
+    # для четвертого канала 0b00001111 - ADSB-112; 0b00001011 - ADSB-56; 0b00000111 - A/C
+    if (data[6] ^ 0b00001100)==0b00000000:
+        print('first channel')
+    
