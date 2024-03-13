25d792f5294959b5e5f118cdf85d6904ebf8fef4
Misha Treushnikov <treushnikovmv@ya.ru>
2024-03-13 16:18:31 +0300

    4

--- .myenv/udp.py
+++ .myenv/udp.py
@@ -1,7 +1,7 @@
 import socket
 import math
 import myFunc
-from crc import Calculator, Crc8
+from crc import Calculator, Crc16
 
 UDP_IP = "10.1.1.16"
 UDP_PORT = 49002
@@ -20,19 +20,30 @@ while True:
         line_str += 'number of receiver  '+str(int(data[2:4].hex(),16))+'; '
         line_str += 'level  '+str(40-int(data[15:16].hex(),16))+'; ' 
         line_str += 'SNR  '+str(2+20*math.log10(int(data[16:17].hex(),16)))+'; '
-        
+        line_str += 'NumOfPack  '+str(int(data[4:6].hex(),16))+'; '
+        line_str += 'CountOfMess  '+str(int(hex(data[1]),16))+'; '
+
         print (line_str) # проверка: можно ли два байта брать такой записью
 
         print(myFunc.byteToTypeAndNumberOfChannel(data[6]))
-        calc = Calculator(Crc8.CCITT)
-        chckSm = calc.checksum(data[:-1])
-        print((chckSm))
+        calc = Calculator(Crc16.CCITT)
+        chckSm = calc.checksum(data[0:len(data)-2])
+        #print((chckSm))
 
         if (data[6] ^ 0b00001100)==0b00000000:
-            adsb_112_Data = data[17:31]
-            print(adsb_112_Data.hex())
-            print('ICAOaddress   '+adsb_112_Data[1:4].hex())
-            numOf112 +=1
+            adsb_112_Data = data[17:31]         
+            if ((adsb_112_Data[0]>>2) ^ 0b100011)==0b00000: # проверка df17 - первые шесть бит первого байта
+                print(adsb_112_Data.hex())
+                ME_in_bytes = adsb_112_Data[4:11]
+                #print(ME_in_bytes.hex())
+                if ((ME_in_bytes[2] | 0b11111011) ^ 0b11111111) == 0b00000000: #смотрим 6-ой бит в третьем байте MESSAGE
+                    print ("CPR odd (1)")
+                else:
+                    print ("CPR even (0)") 
+                print('ICAOaddress   '+adsb_112_Data[1:4].hex())
+                numOf112 +=1
+            else:
+                print((adsb_112_Data[0]>>2))
         
-        if numOf112 >9:
+        if numOf112 >10:
             break
