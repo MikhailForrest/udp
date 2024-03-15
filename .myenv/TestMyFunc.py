f9b52e14885dc58f2a52f77f7b771ef94b5ae891
Misha Treushnikov <treushnikovmv@ya.ru>
2024-03-15 22:26:50 +0300

    5

---
+++ .myenv/TestMyFunc.py
@@ -0,0 +1,10 @@
+import unittest
+import myFunc
+
+class TestMyFunc(unittest.TestCase):
+    def test_byteToTypeAndNumberOfChannel(self):
+        self.assertEqual(myFunc.byteToTypeAndNumberOfChannel(0b00000100),'first channel - A/C')
+
+# Executing the tests in the above test case class
+if __name__ == "__main__":
+  unittest.main()
