b56590281e8a58735e39ec5844159cbb4d9db8e3
Misha Treushnikov <treushnikovmv@ya.ru>
2024-03-28 22:00:52 +0300

    28032024

--- .myenv/TestMyFunc.py
+++ .myenv/TestMyFunc.py
@@ -6,7 +6,8 @@ class Test_MyFunc(unittest.TestCase): # py testmyfunc.py -v
     def test_byteToTypeAndNumberOfChannel(self):
         self.assertEqual(myFunc.byteToTypeAndNumberOfChannel(0b00000100),'first channel - A/C')
 
-
+    def test_TC11Message(self):
+       self.assertEqual(myFunc.TC11Message(bytes([0x58,0x1B,0x66,0xE9,0xBD,0x8C,0xEE]))['n_lat_cpr'], 95454)
 
 # Executing the tests in the above test case class
 if __name__ == "__main__":
