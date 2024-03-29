dad55150b3557a620dfacce485a8c8eeff56b697
Misha Treushnikov <treushnikovmv@ya.ru>
2024-03-29 22:27:29 +0300

    29032024

--- .myenv/TestMyFunc.py
+++ .myenv/TestMyFunc.py
@@ -9,7 +9,7 @@ class Test_MyFunc(unittest.TestCase): # py testmyfunc.py -v
     def test_TC11Message(self):
        self.assertEqual(myFunc.TC11Message(bytes([0x58,0x1B,0x66,0xE9,0xBD,0x8C,0xEE]))['n_lat_cpr'], 95454)
        self.assertEqual(myFunc.TC11Message(bytes([0x58,0x1B,0x66,0xE9,0xBD,0x8C,0xEE]))['n_lon_cpr'], 101614)
-       
+       self.assertEqual(myFunc.TC11Message(bytes([0x58,0x1B,0x66,0xE9,0xBD,0x8C,0xEE]))['format'], 1)
 
 # Executing the tests in the above test case class
 if __name__ == "__main__":
